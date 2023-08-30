667a03c8d6dfc2d40db4d0116c41f3b2ec6dba48
thezzisu <thezzisu@gmail.com>
2023-08-31 01:48:16 +0800

    chore: fix hostname trim

--- src/main.rs
+++ src/main.rs
@@ -38,7 +38,7 @@ fn get_hostname() -> String {
         .unwrap_or_else(|e| panic!("failed to execute process: {}", e));
 
     if output.status.success() {
-        String::from_utf8(output.stdout).unwrap()
+        String::from_utf8(output.stdout).unwrap().trim().to_string()
     } else {
         String::from("unknown")
     }
